e7112882529a03353febd6cb4426bfc69a3a07bd
grenaud <gabriel.reno@gmail.com>
2022-07-25 19:39:13 +0200

    plot

--- plotDist.R
+++ plotDist.R
@@ -7,7 +7,7 @@ data_array <- read.table("array.time_parsed");
 
 
 png("array_vs_bit.png");
-plot(density(data_array$V1),xlab="Runtime",ylab="Density",main="Runtime array vs bit operators",col="darkblue");
+plot(density(data_array$V1),xlab="Runtime (s)",ylab="Density",main=paste("Runtime array (ave:",sprintf("%.3f",mean(data_array$V1)),") vs bit operators (ave:",sprintf("%.3f",mean(data_bit$V1)),")"),col="darkblue");
 lines(density(data_bit$V1),col="darkred");
 legend("topright", title="method", c("array","bitops"), lty=c(1), lwd=c(2.5),col=c("darkblue","darkred")) 
 dev.off();
